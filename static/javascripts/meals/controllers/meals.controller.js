f4f796656113945b857c0c1942290af729ef7fca
George London <george.j.london@gmail.com>
2015-06-25 17:13:27 -0700

    adds unit tests covering key functionality of meals controller and refactors meals controller to be more atomic and testable

--- static/javascripts/meals/controllers/meals.controller.js
+++ static/javascripts/meals/controllers/meals.controller.js
@@ -13,17 +13,21 @@
         .module('mealTracker.meals.controllers')
         .controller('MealsController', MealsController);
 
-    MealsController.$inject = ['$scope', '$filter', "$location", "$timeout", "Profile", "Authentication"];
+    MealsController.$inject = ['$scope', '$filter', "$timeout"];
 
     /**
      * @namespace MealsController
      */
-    function MealsController($scope, $filter, $location, $timeout, Profile, Authentication) {
+    function MealsController($scope, $filter, $timeout) {
         var vm = this;
 
         vm.columns = [];
         vm.visibleMeals = [];
         vm.filterMeals = filterMeals;
+        vm.calculate_visible_calories = calculate_visible_calories;
+        vm.calculate_visible_meals = calculate_visible_meals;
+        vm.sortMealsByTime = sortMealsByTime;
+        vm.findDateRangeOfMeals = findDateRangeOfMeals;
         vm.activate = activate;
         vm.activate();
 
@@ -70,15 +74,12 @@
 
             $scope.$watchCollection(function () { return $scope.meals; }, updateMeals);
             $scope.$watchCollection(function () { return $scope.profile; }, updateDailyCalorieTarget);
-            // TODO this requires a different render function
-            //$scope.$watch(function () { return $(window).width(); }, render);
             $scope.$watch(function () { return vm.visibleInterval }, updateTargetIntervalCalories);
             $scope.$watch(function () { return vm.dailyCalorieTarget }, updateTargetIntervalCalories);
             $scope.$watch(function () { return vm.dateTimeRange.date.from; }, filterMeals);
             $scope.$watch(function () { return vm.dateTimeRange.date.to; }, filterMeals);
             $scope.$watch(function () { return vm.dateTimeRange.time.from; }, filterMeals);
             $scope.$watch(function () { return vm.dateTimeRange.time.to; }, filterMeals);
-            console.log("ACTIVATE");
         }
 
         function updateTargetIntervalCalories(current, original){
@@ -87,62 +88,82 @@
 
         function updateDailyCalorieTarget(){
             vm.dailyCalorieTarget = $scope.profile.calorie_target;
-            console.log("SET cal targ to " + vm.dailyCalorieTarget);
             filterMeals($scope.meals, []);
         }
 
-        function updateMeals(current, original){
-            function sortMealsByTime(a, b) {
-                var timeA = moment(a.meal_time);
-                var timeB = moment(b.meal_time);
-                if (timeA.isBefore(timeB))
-                    return -1;
-                if (timeA.isAfter(timeB))
-                    return 1;
-                return 0;
+        function sortMealsByTime(a, b) {
+            var timeA = moment(a.meal_time);
+            var timeB = moment(b.meal_time);
+            if (timeA.isBefore(timeB))
+                return -1;
+            else if (timeA.isAfter(timeB))
+                return 1;
+            else if (a.name < b.name) {
+                return -1;
             }
+            else{
+                return 1
+            }
+        }
 
-            function findDateRangeOfMeals(meals){
-                var earliestDate = moment();
-                var latestDate = moment();
-                for (var i = 0; i < meals.length; i++) {
-                    var mealTime = moment(meals[i].meal_time);
-                    if (mealTime.isBefore(earliestDate)){
-                        earliestDate = mealTime;
-                    }
-                    if (mealTime.isAfter(latestDate)){
-                        latestDate = mealTime;
-                    }
+        function findDateRangeOfMeals(meals){
+            var earliestDate = moment();
+            var latestDate = moment();
+            for (var i = 0; i < meals.length; i++) {
+                var mealTime = moment(meals[i].meal_time);
+                if (mealTime.isBefore(earliestDate)){
+                    earliestDate = mealTime;
                 }
-                return {
-                    'latestDate': latestDate,
-                    'earliestDate': earliestDate
-                };
+                if (mealTime.isAfter(latestDate)){
+                    latestDate = mealTime;
+                }
             }
+            return {
+                'latestDate': latestDate,
+                'earliestDate': earliestDate
+            };
+        }
+
+        function updateMeals(current, original){
 
             function setDateFilterRange() {
                 vm.visibleMeals = current;
                 var mealDateRange = findDateRangeOfMeals(vm.visibleMeals);
                 vm.dateTimeRange.date.from = mealDateRange.earliestDate;
                 vm.dateTimeRange.date.to = mealDateRange.latestDate;
-                //vm.dateTimeRange.time.from = 1;
-                //vm.dateTimeRange.time.to = 1435;
             }
-            console.log("UPDATE MEALS");
-            console.log("C" + current);
-            console.log("O" + original);
             if (current != original && current.length > 0) {
-                console.log(current);
                 current.sort(sortMealsByTime);
                 setDateFilterRange();
                 filterMeals(current, original);
             }
         }
 
+        function calculate_visible_calories(visibleMeals, username) {
+            return visibleMeals.reduce(function (a, b) {
+                if (b.eater.username === username) {  // make sure admin users only tally their own calories
+                    return a + b.calories;
+                }
+                else {
+                    return a;
+                }
+            }, 0);
+        }
+
+        function calculate_visible_meals(mealsInScope, dateFrom, dateTo, timeFrom, timeTo) {
+            return $filter('filter')(mealsInScope, function (meal, index, array) {
+                var mealDate = moment(meal.meal_time);
+                var mealTime = mealDate.hours() * 60 + mealDate.minutes();
+                var withinDates = mealDate.isBetween(dateFrom, dateTo) || mealDate.isSame(dateFrom) || mealDate.isSame(dateTo);
+                var withinTimes = mealTime >= timeFrom && mealTime <= timeTo;
+                return withinDates && withinTimes;
+            });
+        }
+
         function filterMeals(current, original){
             if (current != original){
+                var mealsInScope = $scope.meals;
                 var username = $scope.profile.username;
-                console.log("filter on " + username);
                 var previousMeals = vm.visibleMeals;
                 var dateFrom = moment(vm.dateTimeRange.date.from).hour(0).minute(0).second(0); // dates have time associated and we want to strip that
                 var dateTo = moment(vm.dateTimeRange.date.to).hour(23).minute(59).second(59);
@@ -150,26 +171,10 @@
                 var timeTo = vm.dateTimeRange.time.to;
                 var daysInInterval = (dateTo.unix() - dateFrom.unix()) / 60 / 60 / 24; // unix time gap in seconds, converted to days
                 var hoursPerDay = ((timeTo - timeFrom) / 60);
+
                 vm.visibleInterval = daysInInterval * hoursPerDay / 24; // in days
-
-                vm.visibleMeals = $filter('filter')($scope.meals, function(meal, index, array){
-
-                    var mealDate = moment(meal.meal_time);
-                    var mealTime = mealDate.hours() * 60 + mealDate.minutes();
-                    
-                    var withinDates = mealDate.isAfter(dateFrom) && mealDate.isBefore(dateTo);
-                    var withinTimes = mealTime >= timeFrom && mealTime <= timeTo;
-                    return withinDates && withinTimes;
-                });
-
-                vm.visibleCalories = vm.visibleMeals.reduce(function(a, b){
-                    if (b.eater.username === username) {  // make sure admin users only tally their own calories
-                        return a + b.calories;
-                    }
-                    else{
-                        return a;
-                    }
-                }, 0);
+                vm.visibleMeals = calculate_visible_meals(mealsInScope, dateFrom, dateTo, timeFrom, timeTo);
+                vm.visibleCalories = calculate_visible_calories(vm.visibleMeals, username);
 
                 render(vm.visibleMeals, previousMeals);
             }
@@ -188,7 +193,6 @@
             if (current !== original) {
                 vm.column = [];
                 for (var i = 0; i < current.length; ++i) {
-
                     vm.column.push(current[i]);
                 }
             }
--- static/javascripts/tests/test-controller.js
+++ static/javascripts/tests/test-controller.js
@@ -1,54 +1,151 @@
 describe('Unit: MealsController', function() {
-  beforeEach(module('mealTracker'));
+    beforeEach(module('mealTracker'));
 
-  var $controller;
-  var $scope;
-  var vm;
+    var $controller;
+    var $scope;
+    var vm;
+    var testData = {};
 
-  beforeEach(inject(function($rootScope, _$controller_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $controller = _$controller_;
-    $scope = $rootScope.$new();
-  }));
+    beforeEach(inject(function ($rootScope, _$controller_) {
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+        $scope = $rootScope.$new();
+    }));
 
-  describe('ViewModel', function() {
+    describe('ViewModel', function () {
 
-    beforeEach(function() {
+        beforeEach(function () {
+            $scope.profile = {
+                "calorie_target": 3500,
+                "email": "george@gmail.com",
+                "id": 11,
+                "is_admin": true,
+                "username": "george"
+            };
+            var otherUser = {
+                "calorie_target": 3500,
+                "email": "james@gmail.com",
+                "id": 11,
+                "is_admin": true,
+                "username": "james"
+            };
+            testData.sampleEarlyMealTime = "2015-03-17T05:19:38";
+            testData.sampleMediumMealTime = "2015-03-21T12:19:38";
+            testData.sampleLateMealTime = "2015-03-25T21:19:38";
+            $scope.meals = [
+                {
+                    "name": "Candy",
+                    "calories": 500,
+                    "description": "",
+                    "meal_time": testData.sampleEarlyMealTime,
+                    "eater": $scope.profile
+                },
+                {
+                    "name": "Pizza",
+                    "calories": 400,
+                    "description": "",
+                    "meal_time": testData.sampleEarlyMealTime,
+                    "eater": otherUser
+                },
+                {
+                    "name": "Hotdog",
+                    "calories": 400,
+                    "description": "",
+                    "meal_time": testData.sampleMediumMealTime,
+                    "eater": $scope.profile
+                },
+                {
+                    "name": "Burrito",
+                    "calories": 400,
+                    "description": "",
+                    "meal_time": testData.sampleLateMealTime,
+                    "eater": $scope.profile
+                }
+            ];
+            vm = $controller('MealsController', {$scope: $scope});
+            $scope.vm = vm;
+            $scope.$digest();
+        });
 
-      vm = $controller('MealsController', { $scope: $scope });
-      $scope.vm = vm;
-      $scope.meals = [];
-      vm.activate();
-      $scope.$digest();
+        it('it updates visible meals when meals in scope changes', function () {
+            var sampleMeals = [
+                {
+                    "name": "Pizza",
+                    "calories": 500,
+                    "description": "",
+                    "meal_time": testData.sampleMediumMealTime,
+                    "eater": $scope.profile
+                }
+            ];
+            $scope.meals = sampleMeals;
+            $scope.$digest();
+            expect(vm.visibleMeals).toEqual(sampleMeals);
+        });
+
+        it('it correctly calculates visible calories', function () {
+            var visibleCalories = vm.calculate_visible_calories($scope.meals, "george");
+            expect(visibleCalories).toEqual(1300);
+        });
+
+        it('it correctly sorts meals by time', function () {
+            var reversedMeals = Array.prototype.slice.call($scope.meals).reverse();
+            reversedMeals.sort(vm.sortMealsByTime);
+            expect(reversedMeals).toEqual($scope.meals);
+        });
+
+        it('it correctly finds min/max date range of array of meals', function () {
+            var dateRange = vm.findDateRangeOfMeals($scope.meals);
+            var latestDate = dateRange.latestDate;
+            var sampleLatestDate = moment(testData.sampleLateMealTime);
+            var earliestDate = dateRange.earliestDate;
+            var sampleEarliestDate = moment(testData.sampleEarlyMealTime);
+            expect(latestDate.isAfter(sampleLatestDate) || latestDate.isSame(sampleLatestDate)).toBeTruthy();
+            expect(earliestDate.isBefore(sampleEarliestDate) || earliestDate.isSame(sampleEarliestDate)).toBeTruthy();
+        });
+
+        it('it correctly filters visible meals based on selected date/time range', function () {
+
+            var earlyDate = moment(testData.sampleEarlyMealTime).subtract(1, 'days');
+            var lateDate = moment(testData.sampleLateMealTime).add(1, 'days');
+            var midDate = moment(testData.sampleMediumMealTime);
+
+            var earliestTime = 0;
+            var latestTime = 1440;
+            var visibleMeals = vm.calculate_visible_meals($scope.meals, earlyDate, lateDate, earliestTime, latestTime);
+            expect(visibleMeals).toEqual($scope.meals);
+
+            visibleMeals = vm.calculate_visible_meals($scope.meals, earlyDate, midDate, earliestTime, latestTime);
+            expect(visibleMeals).toEqual($scope.meals.slice(0,3));
+
+            visibleMeals = vm.calculate_visible_meals($scope.meals, midDate, lateDate, earliestTime, latestTime);
+            expect(visibleMeals).toEqual($scope.meals.slice(2,4));
+
+            var lateTime = 1260;
+
+            visibleMeals = vm.calculate_visible_meals($scope.meals, earlyDate, lateDate, lateTime, latestTime);
+            expect(visibleMeals).toEqual($scope.meals.slice(3,4));
+        });
+
+        it('it updates calorie targets when dailyCalorieTarget changes', function () {
+            var sampleMeals = [
+                {
+                    "name": "Pizza",
+                    "calories": 500,
+                    "description": "",
+                    "meal_time": testData.sampleMediumMealTime,
+                    "eater": $scope.profile
+                },
+                {
+                    "name": "Hotdog",
+                    "calories": 500,
+                    "description": "",
+                    "meal_time": testData.sampleMediumMealTime,
+                    "eater": $scope.profile
+                }
+            ];
+            $scope.meals = sampleMeals;
+            $scope.$digest();
+            expect(vm.visibleMeals).toEqual(sampleMeals);
+        });
     });
-
-    it('it updates visible meals when meals in scope changes', function() {
-      var sampleMeals = [
-        {"name": "Burger", "calories": 500},
-        {"name": "Pizza", "calories": 500},
-        {"name": "Hotdog", "calories": 500}
-        ];
-      $scope.meals = sampleMeals;
-      $scope.$digest();
-      expect(vm.visibleMeals).toEqual(sampleMeals);
-    });
-
-      it('wins life', function() {
-      //$scope.password = 'longerthaneightchars';
-      //$scope.grade();
-      expect(5).toEqual(5);
-    });
-
-      it('wins quuens life', function() {
-      //$scope.password = 'longerthaneightchars';
-      //$scope.grade();
-      expect(4).toEqual(4);
-    });
-
-      it('wins qkns life', function() {
-      //$scope.password = 'longerthaneightchars';
-      //$scope.grade();
-      expect(5).toEqual(4);
-    });
-  });
 });
